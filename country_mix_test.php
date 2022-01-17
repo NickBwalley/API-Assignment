a762a08bf0930ceb4fd19a9fb33c7e090d6bc8ca
NickBwalley <nickbwalley@gmail.com>
2022-01-17 23:44:08 +0300

    api assignment

--- country_mix_test.php
+++ country_mix_test.php
@@ -2,4 +2,22 @@
 $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 2. Kwale. – Salim Mvurya – Jubilee";
 
+function kenyanGovernors($county_data){
+    $myNewArray = explode(",", $county_data);
+    print_r($myNewArray);
+};
+
+kenyanGovernors($county_data);
+
+// echo("<br><br>");
+
+// $str = "hello friend";
+
+// $arr1 = str_split($str);
+// $arr2 = str_split($str, 3);
+
+// print_r($arr1);
+// print_r($arr2);
+
+
 ?>
---
+++ screencaster_122790.php
@@ -0,0 +1,36 @@
+<?php
+
+    // function sanitizer($array, $split_string=",", $array_type=false){
+    //     foreach($array as $arr){
+    //         if($array[0]);
+    //     };
+    // }
+
+    $input_array = ["archeNLand Limtded"];
+    
+    function stringToLowerCase(){
+        
+        $input1 = [100, -1, 200, 300 -700, 678];
+        //$test = array(-10,20,40,-30,50,-60);
+
+        for($i=0; $i<5; $i++){
+            if($input1[$i] < 0){
+                $new_array = array();
+                array_push($i, $new_array);
+            }
+        }
+        print_r($new_array);
+
+        // $result = strtolower($input0);
+        // $capitalize_first = ucfirst($result);
+        // print_r($capitalize_first);
+        // $debits = array();
+    }
+    stringToLowerCase();
+    
+
+    
+    // $input1 = ["archeNland Limtded", ["$100", '-1', '200', '300', '-700', '678']];
+    
+
+?>
