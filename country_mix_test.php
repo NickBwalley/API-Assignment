ef2cdebd40cc2ee09cc8ae7cc78769c60a918ba5
NickBwalley <nickbwalley@gmail.com>
2022-01-18 13:50:41 +0300

    api work

--- country_mix_test.php
+++ country_mix_test.php
@@ -3,8 +3,30 @@ $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 2. Kwale. – Salim Mvurya – Jubilee";
 
 function kenyanGovernors($county_data){
-    $myNewArray = explode(",", $county_data);
-    print_r($myNewArray);
+    $array =  preg_replace('/[^A-Za-z0-9\-\(\) ]/', '', $county_data);
+    echo($array) ;
+    // $round = count($county_data);
+    
+    // for($n = 0; $n < $county_data; $n++ ){
+    //     echo($county_data[$n]);
+    //     echo("<br>");
+    // }
+
+
+    /*$myNewArr1 = preg_split("/\,/", $county_data);
+    print_r($myNewArr1);
+    $array2 = preg_split("/\./", $myNewArr1);
+    print_r($array2);*/
+
+    
+    // if($myNewArray = explode(",", $county_data)){
+    //     echo("<br>");
+    // }
+    // $myNewArray2 = implode($myNewArray);
+    // echo($myNewArray2);
+
+    // print_r($myNewArray);
+
 };
 
 kenyanGovernors($county_data);
